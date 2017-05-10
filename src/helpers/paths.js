9dd4d6e67ad59758bd6579bc214bd778a306eaab
Adam Johnston <adam.johnston3679@gmail.com>
2017-05-10 20:08:14 +0100

    moved to appropriate helper file

--- src/helpers/paths.js
+++ src/helpers/paths.js
@@ -1,8 +1,48 @@
+const R = require('ramda')
+const path = require('path')
+
 //    appendExtensions : string > string
 const appendExtensions = path => {
   return `${path}/**/*.?(js|jsx)`
 }
 
+//    splitOnHyphen : string -> array
+const splitOnHyphen = (str) => {
+  return R.split('-', str)
+}
+
+//    upperCaseWords : array -> array
+const upperCaseWords = (words) => {
+  return R.map(word => word.toUpperCase(), words)
+}
+
+//    joinChars : array -> string
+const joinChars = (chars) => {
+  return R.join('', chars)
+}
+
+//    getPathName : string -> string
+const getPathName = (componentPath) => {
+  const pathName = path.basename(componentPath).split('.')
+
+  if (pathName.length > 1) return pathName[0]
+  throw new Error(`path ${componentPath} must include component file`)
+}
+
+//    getComponentName : string -> string
+const getComponentName = (componentPathName) => (
+  R.pipe(
+    splitOnHyphen,
+    upperCaseWords,
+    joinChars
+  )(componentPathName)
+)
+
 module.exports = {
-  appendExtensions
+  appendExtensions,
+  splitOnHyphen,
+  upperCaseWords,
+  joinChars,
+  getPathName,
+  getComponentName
 }
