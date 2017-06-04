5c2aa81de94143337b59d7a9b18ece4c3dc027a1
Adam Johnston <adam.johnston3679@gmail.com>
2017-06-04 12:07:42 +0100

    pass in an already readable name

--- src/templates/component.js
+++ src/templates/component.js
@@ -1,9 +1,6 @@
 const stringify = require('stringify-object')
-const getReadableComponentName = require('../helpers/paths').getReadableComponentName
 
 module.exports = (name, description) => {
-  name = getReadableComponentName(name)
-
   return `export default ${stringify({
     name,
     description,
