34741b34b1e7bfaa43c27ac723caa500fa422a7b
Adam Johnston <adam.johnston3679@gmail.com>
2017-06-04 12:08:35 +0100

    some methods have been removed so no longer need to be tested

--- tests/paths.test.js
+++ tests/paths.test.js
@@ -2,9 +2,7 @@ const pathHelpers = require('../src/helpers/paths')
 const {
   appendExtensions,
   splitOnHyphen,
-  componentCase,
-  joinChars,
-  getComponentPathName
+  componentCase
 } = pathHelpers
 
 describe('#appendExtensions', () => {
@@ -22,31 +20,5 @@ describe('#splitOnHyphen', () => {
 describe('#componentCase', () => {
   test(`given ['hello', 'world'] expect ['Hello', 'World']`, () => {
     expect(componentCase(['hello', 'world'])).toEqual(['Hello', 'World'])
-  })
-})
-
-describe('#joinChars', () => {
-  test(`given ['hello', 'world'] expect 'helloworld'`, () => {
-    expect(joinChars(['hello', 'world'])).toEqual('helloworld')
-  })
-})
-
-describe('#getComponentPathName', () => {
-  test('the name of the component from \'/components/atoms/button.jsx\' should be button', () => {
-    const path = '/components/atoms/button.jsx'
-
-    expect(getComponentPathName(path)).toBe('button')
-  })
-
-  test('the name of the component from \'/components/molecules/dialog.js\' should be button', () => {
-    const path = '/components/molecules/dialog.jsx'
-
-    expect(getComponentPathName(path)).toBe('dialog')
-  })
-
-  test('the name should throw without a file', () => {
-    const path = '/components/molecules/'
-
-    expect(() => getComponentPathName(path)).toThrowError('path /components/molecules/ must include component file')
   })
 })
